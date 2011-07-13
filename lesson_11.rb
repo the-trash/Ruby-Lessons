075670ec8bddad0e68d5d5645b3e6a5d56e4467b
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-13 12:08:11 +0400

    lesson 11

---
+++ lesson_11.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/ruby
+
+massiv = [1,5,89,7,4,11,27]
+
+puts massiv
+
+puts
+
+puts massiv.length
+puts massiv.count
+puts massiv.size
+
+puts
+
+puts massiv.empty?
+puts massiv.nil?
+# rails => puts massiv.blank?
+
+puts
+
+puts massiv[0]
+puts massiv[3]
+puts massiv[6]
+puts massiv[7]
+
+puts
+
+puts massiv[-1]
+puts massiv[-2]
+puts massiv[-3]
+puts massiv[-7]
+puts massiv[-8]
