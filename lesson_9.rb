8a6ffe998aaa6a6fd64c66d805b3e787418760aa
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-13 11:41:37 +0400

    lesson 9

---
+++ lesson_9.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/ruby
+
+10.times do |counter|
+
+  if counter > 5 then
+    puts "number more then 5"
+  elsif counter < 5 then
+    puts "number less then 5"
+  else 
+    puts "number is 5"
+  end
+
+end
