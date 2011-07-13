0080d7dbe361983ac68707f4e8e0e9f4f0c80d6e
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-13 23:42:06 +0400

    lesson 23 - function calling styles

---
+++ lesson_23.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/ruby
+
+puts `clear`
+
+def some_super_function_actions(times)
+  result_1 = 10 * times
+  result_2 = 20 * times
+  result_3 = 20 * times
+
+  result_2 # return result_2
+end
+
+5.times do |counter|
+  # puts(some_super_function_actions(counter + 1))
+  # puts some_super_function_actions(counter + 1)
+  puts some_super_function_actions counter + 1
+end
