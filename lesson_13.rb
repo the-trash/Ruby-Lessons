475f12d77c3b8613b1db55c2926efab378532dbd
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-13 16:09:28 +0400

    lesson 13

---
+++ lesson_13.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+
+massiv = Array.new
+
+massiv.push 1
+massiv.push 3
+massiv.push -67
+massiv.push 100
+massiv[4] = 120
+massiv.push -10
+
+massiv.each do |element|
+  puts "Element of massiv: " + element.to_s
+end
