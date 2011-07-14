eae8b2259d0ad480846272f8a6231fc149412ae5
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-14 11:59:14 +0400

    lesson 33, class self methods

---
+++ lesson_33.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+
+puts `clear`
+
+class Pig
+
+  # variant 1
+  class << self
+    def description
+      "Pig is animal from farm"
+    end
+  end
+
+  #varaint 2
+  def Pig.weight
+    "Pigs are heavy!"
+  end
+
+  #varaint 3
+  def self.voice
+    "Hreu Hreu"
+  end
+
+end
+
+puts Pig.description
+puts Pig.weight
+puts Pig.voice
