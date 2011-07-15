f810e952a0c32c397e606c04849189f98eafc47b
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-15 14:14:06 +0400

    Lesson 35 - New version - Symbols

--- lesson_35.rb
+++ lesson_35.rb
@@ -7,7 +7,7 @@ class Person
   NAMES = ['John', 'Jan', 'Stephan', 'Mary', 'Clode', 'Sharlin', 'Nick']
   SURNAMES = ['Smith', 'Daw', 'Simpson', 'Strawinsky', 'Allban', 'Crow', 'Rurk']
   
-  attr_accessor 'name', 'surname', 'age', 'money'
+  attr_accessor :name, :surname, 'age', 'money'
   
   def initialize(max= 50, min= 10)
     puts
@@ -20,26 +20,23 @@ class Person
     @money = rand(10000)
   end
 
-  # public is default
-  def fullname
-    "Person is: #{@name} #{@surname}"
-  end
-
-  private
-
-  def money
-    @money.to_s + "$"
-  end
-
-  public
-
-  def special_money_mehod
-    "Person can have " + money
-  end
 end
 
 person_1 = Person.new(60, 15)
 puts person_1.inspect
-puts person_1.fullname
-# puts person_1.money # sorry, private method 'money' => NoMethodError
-puts person_1.special_money_mehod
+
+puts
+
+w = 'Hello'
+x = 'Hello'
+
+y = :Hello
+z = :Hello
+
+puts w.object_id
+puts x.object_id
+
+puts
+
+puts y.object_id
+puts z.object_id
