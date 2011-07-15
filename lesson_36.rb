d40b201695d5709c0548ff44512bab817bcf4188
Ilya N. Zykin <zykin-ilya@ya.ru>
2011-07-15 07:48:48 +0400

    lesson 36, privat and protected methods - new sample

--- lesson_36.rb
+++ lesson_36.rb
@@ -2,49 +2,77 @@
 
 puts `clear`
 
-class Person
-  @@total_cash = 0
+class Animal
+  attr_accessor :speech
 
-  attr_accessor :money # add @money = nil and methods 'money' and 'money='
-
-  def add_dollars
-    @money = 0 if money.nil?
-
-    self.money += 100
-    @@total_cash = total_cash_value + 100
+  def initialize
+    @speech = 'Myau! Murrrr!'
   end
 
-  def total_cash_value
-    @@total_cash
+  def speek_with(some_person)
+    puts @speech
+    puts some_person.speech # speech is protected for Person, its can not be call from Animal instance
   end
-
-  def self.total_cash_with_password(password)
-    return @@total_cash if password == 'qwerty'
-    return '0.00$'
-  end
-
-  public    :add_dollars # for anybody
-  private   :money=      # for self only!
-  protected :total_cash_value  # for any Person
-
 end
 
-person_1 = Person.new
-person_1.add_dollars
-person_1.add_dollars
-person_1.add_dollars
-# person_1.money= 1000000 # sorry, it's private
-puts person_1.money
+class Person
+  attr_accessor :money, :emotions, :speech
+
+  def initialize
+    @money = rand(10000)
+    @emotions = ['bad', 'sad', 'fun', 'happy'].shuffle.first
+    @speech = ['Hello', 'Good day!', 'Hi!', 'Oh! Hello, man!', 'Wow! How are you!'].shuffle.first
+  end
+
+  def know_money(password)
+    return @money.to_s + '$' if password == 'qwerty'
+    "Hey, what are you doing?"
+  end
+
+  def speek_with(some_person)
+    puts @speech              # I say
+    puts some_person.speech   # Yes! I can speek with Person!
+  end
+
+  # public is default
+  public :emotions, :emotions=
+
+  # only for self, not for any Person or anyone
+  private :money, :money=
+
+  # for every Person and self
+  protected :speech, :speech=
+end
+
+mr_bill = Person.new
+mr_joe = Person.new
+
+#puts mr_bill.money # private method
+puts mr_bill.emotions
+puts mr_bill.know_money('qwerty')
 
 puts
 
-person_2 = Person.new
-person_2.add_dollars
-person_2.add_dollars
-puts person_2.money
+#puts mr_joe.money # private method
+puts mr_joe.emotions
+puts mr_joe.know_money('blabla')
 
 puts
 
-# puts Person.total_cash # it's protected
-puts Person.total_cash_with_password 'qwerty'
-puts Person.total_cash_with_password 'qazwsx'
+mr_joe.emotions = 'very happy'
+puts mr_joe.emotions
+
+# mr_joe.money = 10000000 # must be private
+puts mr_joe.know_money('qwerty')
+
+puts
+
+# mr_joe.speech = 'Hello' # speech= it is protected
+# puts mr_joe.speech      # speech  it is protected
+mr_joe.speek_with mr_bill
+
+puts
+
+cat = Animal.new
+mr_joe.speek_with cat
+# cat.speek_with mr_joe
